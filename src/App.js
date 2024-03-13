444ab539a044d676c22f658d7b63ec0ccde20412
nokk0 <derachukwu9@gmail.com>
2024-03-13 19:17:51 +0100

    add code implementation for hero and testimonial section

--- src/App.js
+++ src/App.js
@@ -1,60 +1,21 @@
-import React, { useRef, useState } from 'react';
+import React from "react";
+import { Nav } from "./Components/Nav";
+import { Header } from "./Components/Header";
+import Logo from "./Components/Logo";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+
 
 function App() {
-  const inputRef = useRef(null);
-  const resultRef = useRef(null);
-  const [result, SetResult] = useState(0); // Initial state for input value
-
-  const plus = (event) => {
-    event.preventDefault();
-    SetResult( result => result +Number(inputRef.current.value) )
-    
-  };
-  
-  const minus = (event) =>{
-    event.preventDefault();
-    SetResult( result => result - Number(inputRef.current.value) )
-    
-    
-  }
-  const times = (event) =>{
-    event.preventDefault();
-    SetResult( result => result * Number(inputRef.current.value) )
-    
-    
-  }
-  const divide = (event) =>{
-    event.preventDefault();
-    SetResult( result => result / Number(inputRef.current.value) )
-    
-  }
-  const resetInput = (event) =>{
-    event.preventDefault();
-    inputRef.current.value = 0;
-  }
-  const resetResult = (event) =>{
-    event.preventDefault();
-    SetResult(result => result * 0)
-    
-  }
-
   return (
-    <div>
-      <h1> Simplest Working Calculator</h1>
-      <div>
-        <label ref={resultRef}> {result} </label>
-      </div>
-      <input ref={inputRef} placeholder='type a number' />
-      <div>
-        <button  onClick={plus}> Add </button>
-        <button  onClick={minus}> Subtract </button>
-        <button  onClick={times}> Multiply </button>
-        <button  onClick={divide}> Divide </button>
-        <button  onClick={resetInput}> reset input </button>
-        <button  onClick={resetResult}> reset result </button>
-      </div>
-      {/* <h1>{console.log(inputRef)}</h1> */}
-    </div>
+    <Router>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
